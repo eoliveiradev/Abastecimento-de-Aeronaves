2687436145c46e280ffa16ac036957c901c5a3ba
Emanuel-Oliveira <101813364+Emanuel-Oliveira@users.noreply.github.com>
2022-05-07 14:15:01 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+function calculate(){
+    //Input variables
+    const margemEmergencia = 1.3 //30%
+    const lPorKm = document.querySelector('#lPorKm').value;
+    const combustivelNoTanque = document.querySelector('#combustivelNoTanque').value;
+    const capacidadeMaxTanque = document.querySelector('#capacidadeMaxTanque').value;
+    const distanciaTrecho1 = document.querySelector('#distanciaTrecho1').value;
+    const distanciaTrecho2 = document.querySelector('#distanciaTrecho2').value;
+    
+    //Out put containers
+    const aguardando = document.querySelector("#aguardando")
+    const aprovedContainer = document.querySelector('#aproved')
+    const notAprovedContainer = document.querySelector('#not-aproved')
+    //Out put variables if aproved
+    const trechoPrincipalResult = document.querySelector('#trechoPrincipal');
+    const trechoAalternativoResult = document.querySelector('#trechoAlternativo');
+    const trechoCMargemResult = document.querySelector('#trechoCMargem');
+    const qntCombNecessariaResult = document.querySelector('#qntCombNecessaria');
+    const qntAbastecerResult = document.querySelector('#qntAbastecer');
+
+    //Boring math '-'
+    const distanciaTotal = (distanciaTrecho1 + distanciaTrecho2) * margemEmergencia;
+    const combustivelNecessario = distanciaTotal * lPorKm;
+    const combustivelAbastecer = combustivelNecessario - combustivelNoTanque;
+
+    if (combustivelNecessario > capacidadeMaxTanque){
+        aguardando.classList.add('hidden');
+        aprovedContainer.classList.add('hidden');
+        notAprovedContainer.classList.remove('hidden');
+    }
+    else if(combustivelNecessario < capacidadeMaxTanque){
+        aguardando.classList.add('hidden');
+        aprovedContainer.classList.remove('hidden');
+        notAprovedContainer.classList.add('hidden')
+        trechoPrincipalResult.textContent = distanciaTrecho1 + " KM";
+        trechoAalternativoResult.textContent = distanciaTrecho2 + " KM";
+        trechoCMargemResult.textContent = distanciaTotal + " KM";
+        qntCombNecessariaResult.textContent = combustivelNecessario + " L";
+        qntAbastecerResult.textContent = combustivelAbastecer + " L";
+    }
+}
+
+
+
+
+
+
+
+function resetButton(){
+    location.reload()
+}
