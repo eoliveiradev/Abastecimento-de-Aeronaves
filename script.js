dba8bbff22d59848da365db9f8708792c941d0e7
Emanuel-Oliveira <101813364+oZoiko@users.noreply.github.com>
2022-06-07 11:01:15 -0300

    Javascript Update
    The JavaScript code is now a bit more organized.

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
 function calculate(){
     //Input variables
-    const margemEmergencia = 1.3 //30%
-    const lPorKm = parseFloat(document.querySelector('#lPorKm').value);
-    const combustivelNoTanque = parseFloat(document.querySelector('#combustivelNoTanque').value);
-    const capacidadeMaxTanque = parseFloat(document.querySelector('#capacidadeMaxTanque').value);
-    const distanciaTrecho1 = parseFloat(document.querySelector('#distanciaTrecho1').value);
-    const distanciaTrecho2 = parseFloat(document.querySelector('#distanciaTrecho2').value);
+    const input = {
+        margemEmergencia : 1.3 /* 30% */,
+        lPorKm : parseFloat(document.querySelector('#lPorKm').value),
+        combustivelNoTanque : parseFloat(document.querySelector('#combustivelNoTanque').value),
+        capacidadeMaxTanque : parseFloat(document.querySelector('#capacidadeMaxTanque').value),
+        distanciaTrecho1 : parseFloat(document.querySelector('#distanciaTrecho1').value),
+        distanciaTrecho2 : parseFloat(document.querySelector('#distanciaTrecho2').value)
+    }
+
     
     //Out put containers
-    const aguardando = document.querySelector("#aguardando")
-    const aprovedContainer = document.querySelector('#aproved')
-    const notAprovedContainer = document.querySelector('#not-aproved')
-    //Out put variables if aproved
-    const trechoPrincipalResult = document.querySelector('#trechoPrincipal');
-    const trechoAalternativoResult = document.querySelector('#trechoAlternativo');
-    const trechoCMargemResult = document.querySelector('#trechoCMargem');
-    const qntCombNecessariaResult = document.querySelector('#qntCombNecessaria');
-    const qntAbastecerResult = document.querySelector('#qntAbastecer');
+    const output = {
+        aguardando : document.querySelector("#aguardando"),
+        aprovedContainer : document.querySelector('#aproved'),
+        notAprovedContainer : document.querySelector('#not-aproved'),
+        //Out put variables if aproved
+        trechoPrincipalResult : document.querySelector('#trechoPrincipal'),
+        trechoAalternativoResult : document.querySelector('#trechoAlternativo'),
+        trechoCMargemResult : document.querySelector('#trechoCMargem'),
+        qntCombNecessariaResult : document.querySelector('#qntCombNecessaria'),
+        qntAbastecerResult : document.querySelector('#qntAbastecer'),
+    }
+
 
     //Boring math '-'
-    const distanciaTotal = (distanciaTrecho1 + distanciaTrecho2) * margemEmergencia;
-    const combustivelNecessario = distanciaTotal * lPorKm;
-    let combustivelAbastecer = combustivelNecessario - combustivelNoTanque;
+    const distanciaTotal = (input.distanciaTrecho1 + input.distanciaTrecho2) * input.margemEmergencia;
+    const combustivelNecessario = distanciaTotal * input.lPorKm;
+    let combustivelAbastecer = combustivelNecessario - input.combustivelNoTanque;
     
     if (combustivelAbastecer < 0){
         combustivelAbastecer = 0;
     }
 
-    if (combustivelNecessario > capacidadeMaxTanque){
-        aguardando.classList.add('hidden');
-        aprovedContainer.classList.add('hidden');
-        notAprovedContainer.classList.remove('hidden');
+
+    //Out put
+    if (combustivelNecessario > input.capacidadeMaxTanque){
+        output.aguardando.classList.add('hidden');
+        output.aprovedContainer.classList.add('hidden');
+        output.notAprovedContainer.classList.remove('hidden');
     }
-    else if(combustivelNecessario < capacidadeMaxTanque){
-        aguardando.classList.add('hidden');
-        aprovedContainer.classList.remove('hidden');
-        notAprovedContainer.classList.add('hidden')
-        trechoPrincipalResult.textContent = distanciaTrecho1 + " KM";
-        trechoAalternativoResult.textContent = distanciaTrecho2 + " KM";
-        trechoCMargemResult.textContent = distanciaTotal + " KM";
-        qntCombNecessariaResult.textContent = combustivelNecessario + " L";
-        qntAbastecerResult.textContent = combustivelAbastecer + " L";
+    else if(combustivelNecessario < input.capacidadeMaxTanque){
+        output.aguardando.classList.add('hidden');
+        output.aprovedContainer.classList.remove('hidden');
+        output.notAprovedContainer.classList.add('hidden')
+        output.trechoPrincipalResult.textContent = input.distanciaTrecho1.toFixed(2) + " KM";
+        output.trechoAalternativoResult.textContent = input.distanciaTrecho2.toFixed(2) + " KM";
+        output.trechoCMargemResult.textContent = distanciaTotal.toFixed(2) + " KM";
+        output.qntCombNecessariaResult.textContent = combustivelNecessario.toFixed(2) + " L";
+        output.qntAbastecerResult.textContent = combustivelAbastecer.toFixed(2) + " L";
     }
 }
-
-
-
-
-
 
 
 function resetButton(){
